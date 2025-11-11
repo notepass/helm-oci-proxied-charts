3e92f6b32c28970f10b341a9abac63e937ce5c83
Your Name <you@example.com>
2025-11-11 16:57:04 +0000

    Add tracker script

---
+++ update-all.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -eo pipefail
+
+function localCacheExists() {
+  local _chartName="$1"
+  local _chartVersion="$2"
+
+  if [ -f "./charts/${_chartName}-${_chartVersion}.tgz" ]
+  then
+    return 0
+  else
+    return 1
+  fi
+}
+
+while read -r repo
+do
+  cd charts
+  chartName="$(basename "$repo")"
+  echo "Updating $repo (Chart: $chartName)"
+  oras repo tags codeberg.org/wrenix/helm-charts/paperless-ngx | grep -Eo '[0-9]\.[0-9]\.[0-9]' | while read -r tag
+  do
+    echo "Found tag $tag in remote repo."
+    if ! localCacheExists "$chartName" "$tag"
+    then
+      echo "Missing local version of $chartName:$tag"
+      helm pull "$repo" --version "$tag"
+    fi
+  done
+  cd ..
+done < ./tracked.txt
+
+echo "Creating index"
+helm repo index charts --url https://notepass.github.io/helm-oci-proxied-charts/charts/
